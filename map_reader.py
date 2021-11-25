7be58bd000c4384a2e1460829b9c989c58fdd1ea
Viatlii Zavadskyi <zavadskiy.vs@phystech.edu>
2021-11-25 23:47:05 +0300

    [map] Update examples with colors and rendering

--- map_reader.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-import numpy as np
-
-
-img = Image.open('test.png')
-img = img.convert(mode="1", dither=Image.NONE)
-array = np.array(img, dtype=np.uint8)
-print(array)
---
+++ pygame-animation/example-map/map_reader.py
@@ -0,0 +1,29 @@
+from PIL import Image
+import numpy as np
+
+
+objects = {
+    (255, 255, 255): "*",
+    (0, 0, 0): "+",
+    (0, 255, 0): ".",
+    (255, 0, 0): "A",
+}
+
+img = Image.open('test.png')
+img = img.convert(mode="1", dither=Image.NONE)
+array = np.array(img, dtype=np.uint8)
+
+print("Raw map: ")
+print(array)
+
+img = Image.open('test.png')
+array = np.array(img, dtype=np.uint8)
+
+res = []
+for i, row in enumerate(array):
+    tmp = ""
+    for j, el in enumerate(row):
+        tmp += objects[tuple(el)]
+    res.append(tmp)
+print("After 'render': ")
+print("\n".join(res))
